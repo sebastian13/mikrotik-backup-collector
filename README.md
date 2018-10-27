8c821b9a1d6aef9047cd029c00b0d6e11008debe
Sebastian Plocek <sebastian@plocek.at>
2018-10-27 23:46:30 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# mikrotik-backup-collector
-Script to save Mikrotik RouterOS Backups via SSH to a central location
+# Mikrotik RouterOS Backup Collector
+This script collects Mikrotik RouterOS Exports and Backups via SSH to a central location. It supports [port knocking](https://wiki.mikrotik.com/wiki/Port_Knocking) for enhanced security. It connects to remote Mikrotik via SSH, saves an */export compact* and */backup* unencrypted. To run this script, a SSH KEY is necessary. A dedicated backup user should be created on the router. 
+
+
+# How to use
+
+1. Create a *hosts.txt* file, listing all your Mikrotik Routers
+
+	```
+	10.12.13.14         my-local-router
+	remote.example.com  my-remote-router
+	# 8.8.8.8           backup-later
+	```
+
+1. (Optional) Define custom Environment Variables in a file named *.env*
+
+	```
+	PRIVKEY="~/.ssh/id_rsa"
+	SSH_PORT="1234"
+	KNOCK01="1234"
+	KNOCK02="5678"
+	```
+
+1. Run the script. The argument `--initial` is helpful at the first run. It will add the remote SSH fingerprints to *~/.ssh/known_hosts.*. The argument `--parallel` will do the colection jobs in parallel.
+
+	```bash
+	./mikrotik-backup-collector.sh \
+	  --initial \       # to accept new SSH fingerprints
+	  --parallel        # if you're in a hurry
+	```
---
+++ mikrotik-backup-collector.sh
@@ -0,0 +1,90 @@
+#!/bin/bash
+
+# -------------------------------------------------------
+#   Mikrotik Backup Collector
+# -------------------------------------------------------
+
+## Exit if any statement returns a non-true value
+set -e
+
+## Initialize Variables
+WORKDIR="$(dirname "$(readlink -f "$0")")" 
+HOSTS="$WORKDIR/hosts.txt"
+PRIVKEY="$WORKDIR/.ssh/id_rsa"
+FILENAME=`date +\%Y\%m\%d`
+SSH_PORT="22"
+KNOCK01=""
+KNOCK02=""
+
+## Load your custom Variables, if provided
+[ -r "$WORKDIR/.env" ] && source "$WORKDIR/.env"
+
+## Loop to load arguments
+while [[ $# -gt 0 ]]
+do
+  case "$1" in
+	--initial )      INIT=true; shift; ;;	# To add hosts to SSH known hosts file
+	--parallel )     PARA=true; shift; ;;	# To run backup tasks simultaneously
+	-- )             shift; break ;;
+    * )              shift; ;;
+  esac
+done
+
+# Create/Append logfile
+date | tee -a $WORKDIR/backup.log
+
+## Main Backup Task
+function backup {
+	backup_dir="${WORKDIR}/${alias}/`date +\%Y\%m`"
+	mkdir -p $backup_dir
+
+	# echo "---------------"
+	# echo "Starting Backup of" $alias
+
+	# Export Compact
+	# Use SSH -n and SCP </dev/null to prevent eating up the actionlist
+	ssh -n backup@${ip} -i $PRIVKEY -p ${SSH_PORT} /export compact > $backup_dir/$FILENAME.rsc
+
+	# Backup Unencrypted
+	ssh -n -i $PRIVKEY backup@${ip} -p ${SSH_PORT} /system backup save dont-encrypt=yes name=last.backup > /dev/null
+	scp -q -i $PRIVKEY -P ${SSH_PORT} backup@${ip}:/last.backup $backup_dir/$FILENAME.backup </dev/null
+
+	# Check the files ...
+	#	-s			... exist and are not empty
+	#   -nmin 5		... are not older than 5 Minutes
+	test -s $backup_dir/$FILENAME.rsc && test `find "$backup_dir/$FILENAME.rsc" -mmin -5` \
+		&& echo "[Success] $alias: Export-Compact ✓" \
+		| tee -a $WORKDIR/backup.log \
+		|| { echo "[Error] Something's wrong with export compact of $alias"; exit 1; } \
+		| tee -a $WORKDIR/backup.log
+
+	test -s $backup_dir/$FILENAME.backup && test `find "$backup_dir/$FILENAME.backup" -mmin -5` \
+		&& echo "[Success] $alias: Backup ✓" \
+		| tee -a $WORKDIR/backup.log \
+		|| { echo "[Error] Something's wrong with the backup of $alias"; exit 1; } \
+		| tee -a $WORKDIR/backup.log
+}
+
+# Read the provided file
+grep -v '^#' ${HOSTS} | while read ip alias
+do
+	[ -z "$alias" ] && { echo "Missing Alias of ${ip}. Stopping Backup now."; exit 1; }
+	
+	# Knock Knock
+	curl --silent --fail ${ip}:${KNOCK01} ${ip}:${KNOCK02} || : # echo "Knocked on $alias"
+
+	if [ "$INIT" = true ]
+	then
+		# Add SSH Fingerprint to the list of known hosts
+		ssh backup@${ip} -n -i $PRIVKEY -o StrictHostKeyChecking=no -p ${SSH_PORT} /system resource print
+	fi
+
+	if [ "$PARA" = true ]
+	then
+		# Start backups in parallel. Output will be printed with some delay.
+		backup &
+	else
+		# Normal / Sequential runs
+		backup
+	fi
+done
